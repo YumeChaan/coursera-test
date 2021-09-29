af965a2ab47eec2ea1fbebac1e0fe7446d257cfc
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2021-09-29 15:07:59 +0530

    updated

---
+++ site/page9_c.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application Form for Donors</title>
+    <link rel="stylesheet" type="text/css" href="css/page9_c.css">
+</head>
+<body>
+    <div class="container">
+        <div class="title">Application Form for Donors</div><!-- title -->
+        <form action="#">
+            <div class="donor-details">
+                
+                <div class="input-box">
+                    <span class="detail">Full Name</span>
+                    <input type="text" placeholder="Enter Your Fullname" required>
+                </div><!-- input-box -->
+
+                <div class="input-box">
+                    <span class="detail">NIC Number</span>
+                    <input type="text" placeholder="Enter Your NIC Number" required>
+                </div><!-- input-box -->
+
+                <div class="input-box">
+                    <span class="detail">Age</span>
+                    <input type="number" placeholder="Enter Your Age" required>
+                </div><!-- input-box -->
+
+                <div class="input-box">
+                    <span class="detail">Address</span>
+                    <input type="address" placeholder="Enter Your Address" required>
+                </div><!-- input-box -->
+
+                <div class="input-box">
+                    <span class="detail">Contact Number</span>
+                    <input type="tel" placeholder="Enter Your Contact Number" required>
+                </div><!-- input-box -->
+
+            </div><!-- donor-details -->
+
+            <div class="button">
+                <input type="submit" value="Submit">
+            </div><!-- button -->
+        </form>
+    
+    </div><!-- container -->
+</body>
+</html>
